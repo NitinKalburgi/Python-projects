4e5cacf3bc913958e1a167d0a4c63e3e927ccfa0
Nitin Kalburgi <72649996+NitinKalburgi@users.noreply.github.com>
2021-11-30 22:07:31 +0530

    Add files via upload

---
+++ BandName.py
@@ -0,0 +1,20 @@
+# First let us display a welcoming message 
+print("Welcome to the Band Name Generator!")
+
+# Its time to ask them where they have grown up!!!
+
+city = input("What's the name of the city you grew up in ?\n")
+
+# Time to know about ther pet name if they have any or the name they would prefer if they had pet!!!
+
+pet = input("What's your pet's name ?\n")
+
+print("Your Band Name could be",city,pet)
+
+
+'''
+Now you might be wondering what that "input" does..It takes an input through the user and stores in in the variable
+The catch is it can be onlt a string or a character of you want to store any other data type you can edit it as following
+something = datatype(input("Hola"))
+ex : integer = int(input("What's your age ?"))
+'''
